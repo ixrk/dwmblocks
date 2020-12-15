0e46e88770b6988ce3afeb0960f0d2d7d9e240b7
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2020-12-16 00:13:14 +0100

    Change commands to scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🐧", "uname -r",					                                    0,	0},
-	{"VOL ", "pamixer --get-volume-human",	                                30,	1},
-    {"", "netrate.sh",                                                      1,  0},
-	{"CPU ", "printf '%.2f, %d°C' $(cut -d' ' -f1 /proc/loadavg) $(($(cat /sys/class/thermal/thermal_zone2/temp) / 1000))",	2,	0},
-	{"", "date '+%a %F %H:%M'",					    	                    30,	0},
+	{"🐧", "statuskernel",					    0,	0},
+	{"VOL ", "statusvolume",	                30,	1},
+    {"", "statusnetrate",                       1,  0},
+	{"CPU ", "statuscpu",						2,	0},
+    {"", "statuscharge",                        10, 8},
+	{"", "statusdate",						    30,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
